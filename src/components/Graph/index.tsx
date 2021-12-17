4dabbdccd8c92049aac769bb45a0304bf26724f4
Willian Lins Araújo <willian.araujo@anchieta.br>
2021-12-17 17:46:37 -0300

    ok

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -1,18 +1,48 @@
-
+import { Bar } from 'react-chartjs-2'
+import { Chart, registerables } from 'chart.js';
 
 import styles from "./styles.module.scss";
 
 
 export function Graph() {
   
- 
+  Chart.register(...registerables);
+
+  const state = {
+    labels: ['January', 'February', 'March',
+      'April', 'May','June', 'July', 'August'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: '#6C5DD3',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 0,
+        data: [65, 59, 80, 81, 56, 20, 70, 90],
+        barPercentage: 0.2,
+        minBarLength: 3
+      }
+    ]
+  }
 
   return (
     <div className={styles.graphContainer}>
       <h2>Sales Report</h2>
       <div className={styles.graphCart}>
-        <img src="/assets/chart-3.svg"/>
-        <img src="/assets/chart-1.svg"/>
+        <Bar 
+          data={state}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+              },
+              
+              legend: {
+                display: true,
+                position: 'right'
+              }
+            }
+          }}
+        />
       </div>
     </div>
   );
